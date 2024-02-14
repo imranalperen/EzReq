1de031db9ebc04d20ca13731e141feb2ff2aa9c7
imranalperen <imranalperenbayram@gmail.com>
2024-02-14 23:15:42 +0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,150 @@
+const success_HTTP_statusses = [200, 201];
+class EzReq {
+  #api_url = null;
+  #headers = null;
+
+  get api_url() {
+    return this.#api_url;
+  }
+
+  get headers() {
+    return this.#headers;
+  }
+
+  constructor(api_url, headers) {
+    this.#api_url = new URL(api_url);
+    this.#headers = headers;
+    EzReq.assert(this.#api_url);
+  }
+
+  static assert(url) {
+    if (!(url instanceof URL)) {
+      throw new Error("api url must be instance of URL class");
+    }
+  }
+
+  updateUrl(args) {
+    if (!args.path) {
+      throw new Error("need a pathname");
+    }
+    this.#api_url.pathname = args.path;
+
+    if (args.params) {
+      for (const param in args.params) {
+        this.#api_url.searchParams.append(param, args.params[param]);
+      }
+    }
+
+    if (args.hash) {
+      this.#api_url.hash = args.hash;
+    }
+  }
+
+  async GET(args) {
+    this.updateUrl(args);
+    const raw_response = await fetch(this.#api_url, {
+      method: "GET",
+      headers: args.headers || this.#headers,
+    });
+    return await this.handleResponse(raw_response, args.callback);
+  }
+
+  async POST(args) {
+    this.updateUrl(args);
+    const raw_response = await fetch(this.#api_url, {
+      method: "POST",
+      headers: args.headers || this.#headers,
+      body: JSON.stringify(args.body),
+    });
+    return await this.handleResponse(raw_response, args.callback);
+  }
+
+  async PUT(args) {
+    this.updateUrl(args);
+    const raw_response = await fetch(this.#api_url, {
+      method: "PUT",
+      headers: args.headers || this.#headers,
+      body: JSON.stringify(args.body),
+    });
+    return await this.handleResponse(raw_response, args.callback);
+  }
+
+  async PATCH(args) {
+    await this.PUT(args);
+  }
+
+  async handleResponse(res, callback) {
+    if (success_HTTP_statusses.includes(res.status)) {
+      const response = await res.json();
+      if (callback) {
+        if (typeof callback.fn !== "function") {
+          throw new Error("callback must be a function");
+        }
+        callback.fn(response, callback.args);
+      }
+      return response;
+    } else {
+      const response = res;
+      return response;
+    }
+  }
+}
+
+// ##################################################################
+
+const apiurl = "https://fakestoreapi.com";
+const headers = {
+  "X-access-token": "very-secret-token",
+};
+
+const apiv1 = new EzReq(apiurl, headers);
+
+// if callback have an return value there will be problem
+// first argument is response second one is user arguments
+const callback = (response, your_args) => {
+  console.log(
+    "if token error update token and re request maybe fire a swall or toast bla bla bla",
+  );
+  console.log(your_args);
+};
+
+(async () => {
+  const getRes = await apiv1.GET({
+    path: "products",
+    params: {
+      limit: "5",
+      sort: "desc",
+    },
+    hash: "hello",
+    // headers: {
+    //   lorem: "ipsum",
+    // },
+    // callback: {
+    //   fn: callback,
+    //   args: "this is some cb args",
+    // },
+  });
+  // console.log(getRes);
+
+  const postRes = await apiv1.POST({
+    path: "/products",
+    body: {
+      title: "test product",
+      price: 13.5,
+      description: "lorem",
+      category: "electronic",
+    },
+  });
+  // console.log(postRes);
+
+  const putRes = await apiv1.PUT({
+    path: "/products/2",
+    body: {
+      title: "test product",
+      price: 13.5,
+      description: "update lorem",
+      category: "electronic",
+    },
+  });
+  // console.log(putRes);
+})();
